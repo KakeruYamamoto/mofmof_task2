5b4072059f6309fea98b92eacff77e3c9ab06632
kakeru <okinawan.kkr0713@icloud.com>
2019-11-08 14:04:47 +0900

    Realtorのバリデーション記述

--- app/models/realtor.rb
+++ app/models/realtor.rb
@@ -1,2 +1,10 @@
 class Realtor < ApplicationRecord
+  has_many :nearest_stations
+  accepts_nested_attributes_for :nearest_stations
+  validates :house, presence: true
+  validates :price,presence: true
+  validates :address,presence: true
+  validates :age,presence: true, length: { maximum: 3 }
+  validates :remarkes,presence: true
+  accepts_nested_attributes_for :nearest_stations, allow_destroy: true, reject_if: :all_blank
 end
